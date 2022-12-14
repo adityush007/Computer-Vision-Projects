abf4700f66f23270b9a4163c46bb5939d91b55dc
Aditya Kumar Dubey <adityush007@gmail.com>
2022-12-14 15:10:24 +0530

    Correction for cv2.line(

--- Angle_Measurement.py
+++ Angle_Measurement.py
@@ -6,15 +6,13 @@ pointslst = []
 path = "Resources/acute-angles.png"
 img = cv2.imread(path)
 
-def mousePoints (event, x, y, flags, params):
+def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
         size = len(pointslst)
-        if size != 0 and size %3 == 0:
-            cv2.line(img, tuple(pointslst[round(((size-1)/3)*3)]), (x, y), (0, 0, 255), 2)
-        cv2.circle(img, (x, y), 5, (0, 0, 255), cv2.FILLED)
-        pointslst.append([x, y])
-        print(pointslst)
-        #print(x, y)
+        if size != 0 and size % 3 != 0:
+            cv2.line(img,tuple(pointslst[round((size-1)/3)*3]), (x,y), (0,0,255), 2)
+        cv2.circle(img, (x,y), 5, (0,0,255), cv2.FILLED)
+        pointslst.append([x,y])
 
 def gradient(pt1, pt2):
     return ((pt2[1]-pt1[1])/(pt2[0]-pt1[0]))
